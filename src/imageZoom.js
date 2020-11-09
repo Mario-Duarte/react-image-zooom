8d0ce4050a9a832bf50cd469bcbafe499b0e15cc
Mario-Duarte <djtsunamimario@gmail.com>
2020-11-09 00:27:26 +0000

    feat: adding in initial methods from the jquery plugin over

--- src/imageZoom.js
+++ src/imageZoom.js
@@ -1,12 +1,58 @@
 import React from 'react';
+import styled from "styled-components";
 
-const figureStyle = {
-    
-}
+const Figure = styled.figure`
+    background-position: 50% 50%;
+	position: relative;
+	width: 100%;
+	overflow: hidden;
+	cursor: zoom-in;
+	margin: 0;
+`;
+
+const Img = styled.img`
+    transition: opacity .5s;
+	display: block;
+	width: 100%;
+`;
 
 class ImageZoom extends React.Component {
 
-    componentWillMount() {
+    constructor(props) {
+        super(props);
+        this.state = {
+            zoomed: "1",
+            position: "50% 50%"
+        }
+    }
+
+    zoomIn(e){
+        let zoomer = e.currentTarget;
+        let x,y,offsetX,offsetY;
+        e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
+        e.offsetY ? offsetY = e.offsetY : offsetY = e.touches[0].pageX;
+        x = offsetX/zoomer.offsetWidth*100;
+        y = offsetY/zoomer.offsetHeight*100;
+        this.setState({ position: `${x}% ${y}%`});
+    }
+
+    toggleZoomImage(e) {
+        if ( this.state.zoomed === "0" ) {
+            this.setState({ zoomed: "1" });
+        } else {
+            //zoom in and set the background position correctly
+            this.setState({ zoomed: "0" });
+            this.zoomIn(e);
+        }
+    }
+
+    handleClick(e) {
+        this.toggleZoomImage(e);
+        console.log(this.state.zoomed);
+        //this.setState({ justClicked: letter });
+    }
+
+    componentDidMount() {
         console.log(this.props.zoom);
         if ( this.props.src === "" || this.props.src == null ) {
             throw new Error('Prop src must be defined when using ImageZoom component!');
@@ -15,9 +61,9 @@ class ImageZoom extends React.Component {
 
     render() {
         return(
-            <figure className="containerZoom" style={{backgroundImage: "url("+this.props.src+")", backgroundSize: this.props.zoom+"%"}}>
-				<img id="imageZoom" src={this.props.src} alt={this.props.alt} />
-			</figure>
+            <Figure style={{backgroundImage: "url("+this.props.src+")", backgroundSize: this.props.zoom+"%", backgroundPosition: this.state.position}} onClick={(e) => this.handleClick(e)}>
+				<Img id="imageZoom" src={this.props.src} alt={this.props.alt} style={{opacity: this.state.zoomed}}/>
+			</Figure>
         );
     }
 }
