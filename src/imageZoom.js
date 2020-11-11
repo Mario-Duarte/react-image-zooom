6a5ac3be7dc28be46fce14fd5c49f0cd81a92b8b
Mario-Duarte <djtsunamimario@gmail.com>
2020-11-11 20:33:20 +0000

    feat: Added loading animation and new props

--- src/imageZoom.js
+++ src/imageZoom.js
@@ -1,20 +1,64 @@
 import React from 'react';
-import styled from "styled-components";
+import styled, {keyframes} from "styled-components";
+
+const rotate = keyframes`
+    from { transform: rotate(0deg); }
+    to {transform: rotate(360deg);}
+`;
 
 const Figure = styled.figure`
     background-position: 50% 50%;
 	position: relative;
-    width: 100%;
-    max-width: 600px;
+    width: auto;
 	overflow: hidden;
-	cursor: zoom-in;
-	margin: 0;
+    cursor: zoom-in;
+    min-height: 80px;
+    margin: 0;
+    &:before {
+        content: "";
+        background-color: transparent;
+        position: absolute;
+        top: 0;
+        left: 0;
+        right: 0;
+        width: 100%;
+        height: 100%;
+        opacity: 1;
+        transition: opacity 0.2s ease-in-out;
+        z-index: 1;
+    }
+    &:after {
+        content: "";
+        position: absolute;
+        top: 10px;
+        left: -25px;
+        margin-left: 50%;
+        width: 50px;
+        height: 50px;
+        border-radius: 50%;
+        border: 5px solid transparent;
+        border-top-color: #333;
+        border-right-color: #333;
+        border-bottom-color: #333;
+        animation: ${rotate} 2s linear infinite;
+        opacity: 1;
+        transition: opacity 0.2s ease-in-out;
+        z-index: 2;
+    }
+    &.loaded {
+        min-height: auto;
+        &:before {
+            opacity: 0;
+        }
+        &:after {
+            opacity: 0;
+        }
+    }
 `;
 
 const Img = styled.img`
     transition: opacity .8s;
 	display: block;
-	width: 100%;
 `;
 
 class ImageZoom extends React.Component {
@@ -23,7 +67,8 @@ class ImageZoom extends React.Component {
         super(props);
         this.state = {
             zoomed: "1",
-            position: "50% 50%"
+            position: "50% 50%",
+            imgData: null
         }
     }
 
@@ -74,21 +119,35 @@ class ImageZoom extends React.Component {
         // This checks if the prop src was passed when the component was called and throw an error if this is null or set to empty
         if ( this.props.src === "" || this.props.src == null ) {
             throw new Error('Prop src must be defined when using ImageZoom component!');
-        } 
+        }
+
+        this.setState({ zoomed: "0" });
+        let img = new Image();
+        img.src = this.props.src;
+        img.addEventListener("load", () => {
+            setTimeout(() => {
+                this.setState({ zoomed: "1" });
+                this.setState({ imgData: img.src });
+            }, 200);
+        });
+
     }
 
     render() {
+        let figureClass = this.state.imgData ? 'loaded' : 'loading';
         return(
-            <Figure style={{ backgroundImage: "url("+this.props.src+")", backgroundSize: this.props.zoom+"%", backgroundPosition: this.state.position }} onClick={ e => this.handleClick(e) } onMouseMove={ e => this.handleMove(e) } onMouseLeave={ () => this.handleLeave() }>
-				<Img id="imageZoom" src={this.props.src} alt={this.props.alt} style={{opacity: this.state.zoomed}}/>
+            <Figure id={this.props.id} className={figureClass} style={{ backgroundImage: "url("+this.state.imgData+")", backgroundSize: this.props.zoom+"%", backgroundPosition: this.state.position }} onClick={ e => this.handleClick(e) } onMouseMove={ e => this.handleMove(e) } onMouseLeave={ () => this.handleLeave() }>
+				<Img id="imageZoom" src={this.state.imgData} alt={this.props.alt} style={{opacity: this.state.zoomed}} width={this.props.width} height={this.props.height} />
 			</Figure>
-        );
+            );
     }
 }
 
 ImageZoom.defaultProps = {
     zoom: "200",
-    alt: "This is an imageZoom image"
+    alt: "This is an imageZoom image",
+    width: "100%",
+    height: "auto"
 }
 
 export default ImageZoom
