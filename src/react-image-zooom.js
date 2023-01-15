2052942730d1810bcfd95ac57578bb4f7a9bc83f
Mario Duarte <djtsunamimario@gmail.com>
2023-01-15 13:00:55 +0000

    new version - refactored some code

--- src/react-image-zooom.js
+++ src/react-image-zooom.js
@@ -74,18 +74,9 @@ function ImageZoom(props) {
   function zoomInPosition(e) {
     // this will handle the calculations of the area where the image needs to zoom in depending on the user interaction
     const zoomer = e.currentTarget.getBoundingClientRect();
-    let x, y, offsetX, offsetY;
-    offsetX = e.clientX - zoomer.x;
-    offsetY = e.clientY - zoomer.y;
-    x = (offsetX / zoomer.width) * 100;
-    y = (offsetY / zoomer.height) * 100;
-    setPosition(`${x}% ${y}% `);
-    // OLD JQUERY CODE FOR REF ONLY
-    // e.offsetX ? offsetX = e.offsetX : offsetX = e.touches[0].pageX;
-    // e.offsetY ? offsetY = e.offsetY : offsetY = e.touches[0].pageX;
-    // console.log({offsetX, offsetY});
-    // x = offsetX/zoomer.offsetWidth*100;
-    // y = offsetY/zoomer.offsetHeight*100;
+    let x = ((e.clientX - zoomer.x) / zoomer.width) * 100;
+    let y = ((e.clientY - zoomer.y) / zoomer.height) * 100;
+    setPosition(`${x}% ${y}%`);
   }
 
   function toggleZoomImage(e) {
