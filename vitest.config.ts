9ab4eef5362cb063b1e61aea37ee22a5fffa5b34
Mario-Duarte <djtsunamimario@gmail.com>
2026-02-07 18:11:38 +0000

    Setting up vitest

---
+++ vitest.config.ts
@@ -0,0 +1,13 @@
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react()],
+  test: {
+    environment: "jsdom",
+    exclude: ["./dist", "./node_modules"],
+    coverage: {
+      reporter: ["text", "json", "html"],
+    },
+  },
+});
